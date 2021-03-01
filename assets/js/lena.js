fb91502d2bd0904d9d96197a74e10af463327b92
Henrik Becker <handiman@users.noreply.github.com>
2021-03-01 17:39:25 +0100

    Reverting

--- assets/js/lena.js
+++ assets/js/lena.js
@@ -1,6 +1,6 @@
 (function($) {
   var local = '127.0.0.1' === document.location.hostname;
-  var apiRoot = local ? 'http://localhost:7071/api' : 'https://henrikbeckerapi.azurewebsites.net/api';
+  var apiRoot = local ? 'https://localhost:5001/api/v1' : 'https://henrikbecker.azurewebsites.net/api/v1';
 	$.get(apiRoot + '/ip')
 	  .done(function (ip) {
 	  	$(".contact-form").each(function(){
@@ -8,29 +8,20 @@
 			that.submit(function (e) {
 				e.preventDefault();
 				$(".status", that).html("");
-				$.ajax({
-          type: 'POST',
-          url: apiRoot + '/contact',
-          dataType: 'json',
-          contentType: 'application/json',
-					data: JSON.stringify({
-            subject: 'Contact Form',
-            from: $("#from", that).val(),
-            name: $("#from", that).val(),
-					  address: ip,
-					  message: $("#message", that).val()
-          }),
-          success: function() {
-            $("#from, #message", that).val("");
-            $(".btn", that).html("Skickat!").show();
-            $(".status", that).html("");
-            window.setTimeout(function () { 
-              $(".btn", that).html("Kontakta mig!");
-            }, 10000);
-          },
-          error: function (a, b, c) {
-            $(".status", that).html(c).show();
-          }
+				$.post(apiRoot + '/contact-form', {
+					"subject": "Contact Form",
+					"from": $("#from", that).val(),
+					"address": ip,
+					"message": $("#message", that).val()
+				}).fail(function (a, b, c) {
+					$(".status", that).html(c).show();
+				}).done(function() {
+					$("#from, #message", that).val("");
+					$(".btn", that).html("Skickat!").show();
+					$(".status", that).html("");
+					window.setTimeout(function () { 
+						$(".btn", that).html("Kontakta mig!");
+					}, 10000);
 				});
 				return false;
 			});		
